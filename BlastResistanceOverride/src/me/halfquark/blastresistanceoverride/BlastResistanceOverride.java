ba0f5b61337cf281104d0f48e351af74e2e9e892
HalfQuark <64480337+HalfQuark@users.noreply.github.com>
2021-10-06 21:12:24 +0100

    Misspelling correction
    *looks at roach

---
+++ BlastResistanceOverride/src/me/halfquark/blastresistanceoverride/BlastResistanceOverride.java
@@ -0,0 +1,55 @@
+package me.halfquark.blastresistanceoverride;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlastResistanceOverride extends JavaPlugin{
+	
+	private HashSet<BlockOverride> overrideSet = new HashSet<BlockOverride>();
+	
+	@Override
+	public void onEnable() {
+		this.saveDefaultConfig();
+		getConfig().options().copyDefaults(true);
+		if(getConfig().getConfigurationSection("OverrideValues") == null) {
+			getLogger().info("No override values found.");
+			return;
+		}
+		for(String key : getConfig().getConfigurationSection("OverrideValues").getKeys(false)) {
+			for(Material mat : getBlocks(key)) {
+				BlockOverride bo = new BlockOverride(mat);
+				bo.set("durability", (short) (5 * getConfig().getInt("OverrideValues." + key)));
+				overrideSet.add(bo);
+			}
+		}
+	}
+	
+    public static ArrayList<Material> getBlocks(String name){
+        ArrayList<Material> blocks = new ArrayList<>();
+        if(name.startsWith("#")){
+            Iterable<Tag<Material>> tags = Bukkit.getTags(Tag.REGISTRY_BLOCKS, Material.class);
+            for(Tag<Material> tag : tags){
+                if(tag.getKey().toString().equals(name.substring(1))){
+                    blocks.addAll(tag.getValues());
+                    break;
+                }
+            }
+        }else{
+            blocks.add(Material.matchMaterial(name));
+        }
+        return blocks;
+    }
+	
+	@Override
+	public void onDisable() {
+		for(BlockOverride bo : overrideSet) {
+			bo.revertAll();
+		}
+	}
+	
+}
