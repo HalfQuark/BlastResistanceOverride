0905b81002bb66db456d0c34591695cbc53515d5
HalfQuark <64480337+HalfQuark@users.noreply.github.com>
2021-10-07 15:06:11 +0100

    Tags fixed

--- src/me/halfquark/blastresistanceoverride/BlastResistanceOverride.java
+++ src/me/halfquark/blastresistanceoverride/BlastResistanceOverride.java
@@ -32,7 +32,7 @@ public class BlastResistanceOverride extends JavaPlugin{
 	
     public static ArrayList<Material> getBlocks(String name){
         ArrayList<Material> blocks = new ArrayList<>();
-        if(name.startsWith("#")){
+        if(name.startsWith("$")){
             Iterable<Tag<Material>> tags = Bukkit.getTags(Tag.REGISTRY_BLOCKS, Material.class);
             for(Tag<Material> tag : tags){
                 if(tag.getKey().toString().toLowerCase().equals(name.substring(1).toLowerCase())){
