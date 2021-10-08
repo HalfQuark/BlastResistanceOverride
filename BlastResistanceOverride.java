da73404795f0d6a9cc5d504d272b8381bbd6bae0
goodroach <68086172+goodroach@users.noreply.github.com>
2021-10-07 22:46:57 -1000

    Add files via upload

--- BlastResistanceOverride.java
+++ BlastResistanceOverride.java
@@ -1,11 +1,10 @@
 package me.halfquark.blastresistanceoverride;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Tag;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.minecraft.server.v1_12_R1.Block;
 
 public class BlastResistanceOverride extends JavaPlugin{
 	
@@ -20,32 +19,11 @@ public class BlastResistanceOverride extends JavaPlugin{
 			return;
 		}
 		for(String key : getConfig().getConfigurationSection("OverrideValues").getKeys(false)) {
-			getLogger().info(key);
-			getLogger().info(getBlocks(key).toString());
-			for(Material mat : getBlocks(key)) {
-				BlockOverride bo = new BlockOverride(mat);
-				bo.set("durability", (short) (getConfig().getInt("OverrideValues." + key)));
-				overrideSet.add(bo);
-				getLogger().info(key + " has been successfully registered.");
-			}
+		    BlockOverride bo = new BlockOverride(Block.getById(Integer.parseInt(key)));
+		    bo.set("durability", 5.0f * Float.parseFloat(getConfig().getString("OverrideValues." + key)));
+		    overrideSet.add(bo);
 		}
 	}
-	
-    public static ArrayList<Material> getBlocks(String name){
-        ArrayList<Material> blocks = new ArrayList<>();
-        if(name.startsWith("#")){
-            Iterable<Tag<Material>> tags = Bukkit.getTags(Tag.REGISTRY_BLOCKS, Material.class);
-            for(Tag<Material> tag : tags){
-                if(tag.getKey().toString().equals(name.substring(1))){
-                    blocks.addAll(tag.getValues());
-                    return blocks;
-                } 
-            }
-        }else{
-            blocks.add(Material.matchMaterial(name));
-        }
-        return blocks;
-    }
 	
 	@Override
 	public void onDisable() {
